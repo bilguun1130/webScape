74da96cfc1704f3ede048307bff61e482d0daebf
bilguun1130 <beek1130@gmail.com>
2023-11-03 11:57:49 +0800

    comma added

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function returnValue () {
     for (var i = 0; i < numberChange; i++) {
         pageMaker +=`div.t-row:nth-of-type(${i+1}) a.clickable`;
     }
-    document.getElementById("exampleFormControlTextarea1").value ='{"_id":"'+name+'","startUrl":["https://petition.parliament.mn/petition"],"selectors":[{"id":"wrapper","parentSelectors":["_root"],"type":"SelectorLink","selector":"'+ pageMaker +
+    document.getElementById("exampleFormControlTextarea1").value ='{"_id":"'+name+", "+'","startUrl":["https://petition.parliament.mn/petition"],"selectors":[{"id":"wrapper","parentSelectors":["_root"],"type":"SelectorLink","selector":"'+ pageMaker +
     '","multiple":true,"linkType":"linkFromHref"},{"id":"dugaar","parentSelectors":["wrapper"],"type":"SelectorText","selector":"span.pet-no","multiple":false,"regex":""},{"id":"ovog","parentSelectors":["wrapper"],"type":"SelectorText","selector":".text-dark > :not(strong)","multiple":false,"regex":""},{"id":"tovch Tailbar","parentSelectors":["wrapper"],"type":"SelectorText","selector":"p.fw-normal","multiple":false,"regex":""},{"id":"urgudul","parentSelectors":["wrapper"],"type":"SelectorText","selector":".bg-grad-primary p","multiple":false,"regex":""}]}';
 }
