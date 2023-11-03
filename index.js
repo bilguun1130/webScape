6e1f78e86d6da293af3d5d53faa0db3b57b97d30
bilguun1130 <beek1130@gmail.com>
2023-11-03 12:30:29 +0800

    made it faster

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function returnValue () {
             pageMaker += `div.t-row:nth-of-type(${i+1}) a.clickable`;
         }
     }
-    pageMaker = pageMaker.slice(0, -2); 
+
     document.getElementById("exampleFormControlTextarea1").value ='{"_id":"'+name+'","startUrl":["https://petition.parliament.mn/petition"],"selectors":[{"id":"wrapper","parentSelectors":["_root"],"type":"SelectorLink","selector":"'+ pageMaker +
     '","multiple":true,"linkType":"linkFromHref"},{"id":"dugaar","parentSelectors":["wrapper"],"type":"SelectorText","selector":"span.pet-no","multiple":false,"regex":""},{"id":"ovog","parentSelectors":["wrapper"],"type":"SelectorText","selector":".text-dark > :not(strong)","multiple":false,"regex":""},{"id":"tovch Tailbar","parentSelectors":["wrapper"],"type":"SelectorText","selector":"p.fw-normal","multiple":false,"regex":""},{"id":"urgudul","parentSelectors":["wrapper"],"type":"SelectorText","selector":".bg-grad-primary p","multiple":false,"regex":""}]}';
 }
